4d4e988edcbef6f9e0c8addbe983484093fe09af
guptakshitika-cloud <guptakshitika@gmail.com>
2026-09-14 16:14:51 +0530

    Update number_guessing_game.py

--- number_guessing_game/number_guessing_game.py
+++ number_guessing_game/number_guessing_game.py
@@ -1,13 +1,13 @@
 import random
-x=random.randint(1, 10)
+x=random.randint(1, 10) #picking a random number between 1 and 10
 z = True
 while z == True: 
-  y = input("enter your guess (must be between 1 to 10): ")
+  y = input("enter your guess (must be between 1 to 10): ") #accepting user input
   try:
-    if float(y) in range(1,11):
-      if float(y) == x:
+    if float(y) in range(1,11): #type conversion and checking whether the input is valid
+      if float(y) == x: #what happens if the guess is correct
         z = False
-        print("correct!")
+        print("correct!") 
       else:
         print("Wrong! Try again")
     else:
